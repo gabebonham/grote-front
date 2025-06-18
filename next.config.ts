94adefeb2fa293c27c8d2c365ce5637e2b9cfedd
gabebonham <grotegabriel@gmail.com>
2025-06-18 19:34:38 -0300

    added redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false, // change to true if it's a 301 redirect
+      },
+    ]
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
